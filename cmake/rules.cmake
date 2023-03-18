60a582d807be649d2a9c86a87faa4d70698890c6
José Cruz <jvsc@cin.ufpe.br>
2023-03-18 17:56:40 -0300

    Add macros and compile_commands to robocin's cmake rules

--- cmake/rules.cmake
+++ cmake/rules.cmake
@@ -26,24 +26,26 @@ get_filename_component(ROBOCIN_CONFIG_PATH "~/.config/${ROBOCIN_PROJECT_NAME}" A
 #  SRCS: source files
 #  RSRCS: resource files
 #  DEPS: dependencies
+#  MACROS: macros
+#  COMPILE_OPTIONS: compile options
 function(robocin_cpp_library)
   cmake_parse_arguments(
-          ARG                     # prefix of output variables
-          ""                      # list of names of the boolean arguments
-          "NAME"                  # list of names of mono-valued arguments
-          "HDRS;SRCS;RSRCS;DEPS"  # list of names of multi-valued arguments
-          ${ARGN}                 # arguments of the function to parse (ARGN contains all the arguments after the function name)
+          ARG                                            # prefix of output variables
+          ""                                             # list of names of the boolean arguments
+          "NAME"                                         # list of names of mono-valued arguments
+          "HDRS;SRCS;RSRCS;DEPS;MACROS;COMPILE_OPTIONS"  # list of names of multi-valued arguments
+          ${ARGN}                                        # arguments of the function to parse (ARGN contains all the arguments after the function name)
   )
 
   # if there isn't at least one header file, then the library is not created
-  if(NOT ARG_HDRS)
+  if (NOT ARG_HDRS)
     message(FATAL_ERROR "robocin_cpp_library: no header files given")
-  endif()
+  endif ()
 
   # if there isn't at least one source file, then the library is not created
-  if(NOT ARG_SRCS)
+  if (NOT ARG_SRCS)
     message(FATAL_ERROR "robocin_cpp_library: no source files given")
-  endif()
+  endif ()
 
   add_library(${ARG_NAME} ${ARG_HDRS} ${ARG_SRCS} ${ARG_RSRCS}) # add library with given name, headers, sources and resources
   target_link_libraries(${ARG_NAME} PUBLIC ${ARG_DEPS}) # link library with given dependencies
@@ -55,6 +57,14 @@ function(robocin_cpp_library)
   target_compile_definitions(${ARG_NAME} PRIVATE ROBOCIN_PROJECT_PATH="${ROBOCIN_PROJECT_PATH}")
   target_compile_definitions(${ARG_NAME} PRIVATE ROBOCIN_CONFIG_PATH="${ROBOCIN_CONFIG_PATH}")
 
+  if (ARG_MACROS)
+    target_compile_definitions(${ARG_NAME} ${ARG_MACROS})
+  endif ()
+
+  if (ARG_COMPILE_OPTIONS)
+    target_compile_options(${ARG_NAME} ${ARG_COMPILE_OPTIONS})
+  endif ()
+
 endfunction(robocin_cpp_library)
 
 ########################################################################################################################
@@ -63,24 +73,26 @@ endfunction(robocin_cpp_library)
 # Named parameters:
 #  NAME: name of the library
 #  PROTOS: proto files
+#  MACROS: macros
+#  COMPILE_OPTIONS: compile options
 function(robocin_cpp_proto_library)
   cmake_parse_arguments(
-          ARG               # prefix of output variables
-          ""                # list of names of the boolean arguments
-          "NAME"            # list of names of mono-valued arguments
-          "PROTOS"          # list of names of multi-valued arguments
-          ${ARGN}           # arguments of the function to parse
+          ARG                                      # prefix of output variables
+          ""                                       # list of names of the boolean arguments
+          "NAME"                                   # list of names of mono-valued arguments
+          "PROTOS;MACROS;COMPILE_OPTIONS"          # list of names of multi-valued arguments
+          ${ARGN}                                  # arguments of the function to parse
   )
 
   # if there isn't at least one proto file, then the library is not created
-  if(NOT ARG_PROTOS)
+  if (NOT ARG_PROTOS)
     message(FATAL_ERROR "robocin_cpp_proto_library: no proto files given")
-  endif()
+  endif ()
 
   set(proto_hdrs)
   set(proto_srcs)
 
-  foreach(PROTO ${ARG_PROTOS})
+  foreach (PROTO ${ARG_PROTOS})
     get_filename_component(proto_name ${PROTO} NAME_WE)
     get_filename_component(proto_absolute_file ${PROTO} ABSOLUTE)
     get_filename_component(proto_absolute_path ${proto_absolute_file} DIRECTORY)
@@ -94,8 +106,8 @@ function(robocin_cpp_proto_library)
             OUTPUT "${proto_hdr_file}" "${proto_src_file}"
             COMMAND $<TARGET_FILE:protobuf::protoc>
             ARGS --proto_path ${ROBOCIN_PROJECT_PATH}
-                 --cpp_out "${CMAKE_BINARY_DIR}"
-                 "${proto_relative_file}"
+            --cpp_out "${CMAKE_BINARY_DIR}"
+            "${proto_relative_file}"
             DEPENDS "${proto_absolute_file}"
             WORKING_DIRECTORY ${ROBOCIN_PROJECT_PATH}
     )
@@ -103,7 +115,7 @@ function(robocin_cpp_proto_library)
     list(APPEND proto_hdrs ${proto_hdr_file})
     list(APPEND proto_srcs ${proto_src_file})
 
-  endforeach(PROTO)
+  endforeach (PROTO)
 
   add_library(${ARG_NAME} ${proto_hdrs} ${proto_srcs})
   target_link_libraries(${ARG_NAME} PUBLIC protobuf::libprotobuf) # link library with given dependencies
@@ -115,6 +127,14 @@ function(robocin_cpp_proto_library)
   target_compile_definitions(${ARG_NAME} PRIVATE ROBOCIN_PROJECT_PATH="${ROBOCIN_PROJECT_PATH}")
   target_compile_definitions(${ARG_NAME} PRIVATE ROBOCIN_CONFIG_PATH="${ROBOCIN_CONFIG_PATH}")
 
+  if (ARG_MACROS)
+    target_compile_definitions(${ARG_NAME} ${ARG_MACROS})
+  endif ()
+
+  if (ARG_COMPILE_OPTIONS)
+    target_compile_options(${ARG_NAME} ${ARG_COMPILE_OPTIONS})
+  endif ()
+
 endfunction(robocin_cpp_proto_library)
 
 ########################################################################################################################
@@ -123,26 +143,28 @@ endfunction(robocin_cpp_proto_library)
 # Named parameters:
 #  NAME: name of the library
 #  PROTOS: (grpc) proto files
+#  MACROS: macros
+#  COMPILE_OPTIONS: compile options
 function(robocin_cpp_grpc_library)
   cmake_parse_arguments(
-          ARG               # prefix of output variables
-          ""                # list of names of the boolean arguments
-          "NAME"            # list of names of mono-valued arguments
-          "PROTOS"          # list of names of multi-valued arguments
-          ${ARGN}           # arguments of the function to parse
+          ARG                                      # prefix of output variables
+          ""                                       # list of names of the boolean arguments
+          "NAME"                                   # list of names of mono-valued arguments
+          "PROTOS;MACROS;COMPILE_OPTIONS"          # list of names of multi-valued arguments
+          ${ARGN}                                  # arguments of the function to parse
   )
 
   # if there isn't at least one proto file, then the library is not created
-  if(NOT ARG_PROTOS)
+  if (NOT ARG_PROTOS)
     message(FATAL_ERROR "robocin_cpp_grpc_library: no proto files given")
-  endif()
+  endif ()
 
   set(proto_hdrs)
   set(proto_srcs)
   set(grpc_hdrs)
   set(grpc_srcs)
 
-  foreach(PROTO ${ARG_PROTOS})
+  foreach (PROTO ${ARG_PROTOS})
     get_filename_component(proto_name ${PROTO} NAME_WE)
     get_filename_component(proto_absolute_file ${PROTO} ABSOLUTE)
     get_filename_component(proto_absolute_path ${proto_absolute_file} DIRECTORY)
@@ -158,10 +180,10 @@ function(robocin_cpp_grpc_library)
             OUTPUT "${proto_hdr_file}" "${proto_src_file}" "${grpc_hdr_file}" "${grpc_src_file}"
             COMMAND $<TARGET_FILE:protobuf::protoc>
             ARGS --proto_path ${ROBOCIN_PROJECT_PATH}
-                 --cpp_out "${CMAKE_BINARY_DIR}"
-                 --grpc_out "${CMAKE_BINARY_DIR}"
-                 --plugin=protoc-gen-grpc=$<TARGET_FILE:gRPC::grpc_cpp_plugin>
-                 "${proto_relative_file}"
+            --cpp_out "${CMAKE_BINARY_DIR}"
+            --grpc_out "${CMAKE_BINARY_DIR}"
+            --plugin=protoc-gen-grpc=$<TARGET_FILE:gRPC::grpc_cpp_plugin>
+            "${proto_relative_file}"
             DEPENDS "${proto_absolute_file}"
             WORKING_DIRECTORY ${ROBOCIN_PROJECT_PATH}
     )
@@ -171,7 +193,7 @@ function(robocin_cpp_grpc_library)
     list(APPEND grpc_hdrs ${grpc_hdr_file})
     list(APPEND grpc_srcs ${grpc_src_file})
 
-  endforeach(PROTO)
+  endforeach (PROTO)
 
   add_library(${ARG_NAME} ${proto_hdrs} ${proto_srcs} ${grpc_hdrs} ${grpc_srcs})
   target_link_libraries(${ARG_NAME} PUBLIC protobuf::libprotobuf gRPC::grpc++ gRPC::grpc++_reflection) # link library with given dependencies
@@ -182,6 +204,14 @@ function(robocin_cpp_grpc_library)
   target_compile_definitions(${ARG_NAME} PRIVATE ROBOCIN_PROJECT_NAME="${ROBOCIN_PROJECT_NAME}")
   target_compile_definitions(${ARG_NAME} PRIVATE ROBOCIN_PROJECT_PATH="${ROBOCIN_PROJECT_PATH}")
   target_compile_definitions(${ARG_NAME} PRIVATE ROBOCIN_CONFIG_PATH="${ROBOCIN_CONFIG_PATH}")
+
+  if (ARG_MACROS)
+    target_compile_definitions(${ARG_NAME} ${ARG_MACROS})
+  endif ()
+
+  if (ARG_COMPILE_OPTIONS)
+    target_compile_options(${ARG_NAME} ${ARG_COMPILE_OPTIONS})
+  endif ()
 
 endfunction(robocin_cpp_grpc_library)
 
@@ -194,26 +224,28 @@ endfunction(robocin_cpp_grpc_library)
 #  SRCS: source files
 #  RSRCS: resource files
 #  DEPS: dependencies
+#  MACROS: macros
+#  COMPILE_OPTIONS: compile options
 function(robocin_cpp_test)
   cmake_parse_arguments(
-          ARG                     # prefix of output variables
-          ""                      # list of names of the boolean arguments
-          "NAME"                  # list of names of mono-valued arguments
-          "HDRS;SRCS;RSRCS;DEPS"  # list of names of multi-valued arguments
-          ${ARGN}                 # arguments of the function to parse (ARGN contains all the arguments after the function name)
+          ARG                                            # prefix of output variables
+          ""                                             # list of names of the boolean arguments
+          "NAME"                                         # list of names of mono-valued arguments
+          "HDRS;SRCS;RSRCS;DEPS;MACROS;COMPILE_OPTIONS"  # list of names of multi-valued arguments
+          ${ARGN}                                        # arguments of the function to parse (ARGN contains all the arguments after the function name)
   )
 
   # check if at least one source file is given with suffix '_test.cpp'
-  if(NOT ARG_SRCS)
+  if (NOT ARG_SRCS)
     message(FATAL_ERROR "robocin_cpp_test: no source files given")
-  else()
+  else ()
     set(FILTERED_SRCS ${ARG_SRCS})
     list(FILTER FILTERED_SRCS INCLUDE REGEX "_test\.cpp$")
 
-    if(NOT FILTERED_SRCS)
+    if (NOT FILTERED_SRCS)
       message(FATAL_ERROR "robocin_cpp_test: no source files given with suffix '_test.cpp'")
-    endif()
-  endif()
+    endif ()
+  endif ()
 
   add_executable(${ARG_NAME} ${ARG_HDRS} ${ARG_SRCS} ${ARG_RSRCS}) # add test with given name, headers, sources and resources
   target_link_libraries(${ARG_NAME} PRIVATE GTest::gtest GTest::gtest_main ${ARG_DEPS}) # link library with given dependencies
@@ -227,6 +259,14 @@ function(robocin_cpp_test)
 
   gtest_discover_tests(${ARG_NAME})
 
+  if (ARG_MACROS)
+    target_compile_definitions(${ARG_NAME} ${ARG_MACROS})
+  endif ()
+
+  if (ARG_COMPILE_OPTIONS)
+    target_compile_options(${ARG_NAME} ${ARG_COMPILE_OPTIONS})
+  endif ()
+
 endfunction(robocin_cpp_test)
 
 ########################################################################################################################
@@ -238,26 +278,28 @@ endfunction(robocin_cpp_test)
 #  SRCS: source files
 #  RSRCS: resource files
 #  DEPS: dependencies
+#  MACROS: macros
+#  COMPILE_OPTIONS: compile options
 function(robocin_cpp_benchmark_test)
   cmake_parse_arguments(
-          ARG                     # prefix of output variables
-          ""                      # list of names of the boolean arguments
-          "NAME"                  # list of names of mono-valued arguments
-          "HDRS;SRCS;RSRCS;DEPS"  # list of names of multi-valued arguments
-          ${ARGN}                 # arguments of the function to parse (ARGN contains all the arguments after the function name)
+          ARG                                            # prefix of output variables
+          ""                                             # list of names of the boolean arguments
+          "NAME"                                         # list of names of mono-valued arguments
+          "HDRS;SRCS;RSRCS;DEPS;MACROS;COMPILE_OPTIONS"  # list of names of multi-valued arguments
+          ${ARGN}                                        # arguments of the function to parse (ARGN contains all the arguments after the function name)
   )
 
   # check if at least one source file is given with suffix '_benchmark.cpp'
-  if(NOT ARG_SRCS)
+  if (NOT ARG_SRCS)
     message(FATAL_ERROR "robocin_cpp_benchmark_test: no source files given")
-  else()
+  else ()
     set(FILTERED_SRCS ${ARG_SRCS})
     list(FILTER FILTERED_SRCS INCLUDE REGEX "_benchmark\.cpp$")
 
-    if(NOT FILTERED_SRCS)
+    if (NOT FILTERED_SRCS)
       message(FATAL_ERROR "robocin_cpp_benchmark_test: no source files given with suffix '_benchmark.cpp'")
-    endif()
-  endif()
+    endif ()
+  endif ()
 
   add_executable(${ARG_NAME} ${ARG_HDRS} ${ARG_SRCS} ${ARG_RSRCS}) # add test with given name, headers, sources and resources
   target_link_libraries(${ARG_NAME} PRIVATE benchmark::benchmark benchmark::benchmark_main ${ARG_DEPS}) # link library with given dependencies
@@ -268,6 +310,14 @@ function(robocin_cpp_benchmark_test)
   target_compile_definitions(${ARG_NAME} PRIVATE ROBOCIN_PROJECT_NAME="${ROBOCIN_PROJECT_NAME}")
   target_compile_definitions(${ARG_NAME} PRIVATE ROBOCIN_PROJECT_PATH="${ROBOCIN_PROJECT_PATH}")
   target_compile_definitions(${ARG_NAME} PRIVATE ROBOCIN_CONFIG_PATH="${ROBOCIN_CONFIG_PATH}")
+
+  if (ARG_MACROS)
+    target_compile_definitions(${ARG_NAME} ${ARG_MACROS})
+  endif ()
+
+  if (ARG_COMPILE_OPTIONS)
+    target_compile_options(${ARG_NAME} ${ARG_COMPILE_OPTIONS})
+  endif ()
 
 endfunction(robocin_cpp_benchmark_test)
 
@@ -280,26 +330,28 @@ endfunction(robocin_cpp_benchmark_test)
 #  SRCS: source files
 #  RSRCS: resource files
 #  DEPS: dependencies
+#  MACROS: macros
+#  COMPILE_OPTIONS: compile options
 function(robocin_cpp_executable)
   cmake_parse_arguments(
-          ARG                     # prefix of output variables
-          ""                      # list of names of the boolean arguments
-          "NAME"                  # list of names of mono-valued arguments
-          "HDRS;SRCS;RSRCS;DEPS"  # list of names of multi-valued arguments
-          ${ARGN}                 # arguments of the function to parse (ARGN contains all the arguments after the function name)
+          ARG                                            # prefix of output variables
+          ""                                             # list of names of the boolean arguments
+          "NAME"                                         # list of names of mono-valued arguments
+          "HDRS;SRCS;RSRCS;DEPS;MACROS;COMPILE_OPTIONS"  # list of names of multi-valued arguments
+          ${ARGN}                                        # arguments of the function to parse (ARGN contains all the arguments after the function name)
   )
 
   # check if at least one source file is given with suffix '_main.cpp'
-  if(NOT ARG_SRCS)
+  if (NOT ARG_SRCS)
     message(FATAL_ERROR "robocin_cpp_executable: no source files given")
-  else()
+  else ()
     set(FILTERED_SRCS ${ARG_SRCS})
     list(FILTER FILTERED_SRCS INCLUDE REGEX "_main\.cpp$")
 
-    if(NOT FILTERED_SRCS)
+    if (NOT FILTERED_SRCS)
       message(FATAL_ERROR "robocin_cpp_executable: no source files given with suffix '_main.cpp'")
-    endif()
-  endif()
+    endif ()
+  endif ()
 
   add_executable(${ARG_NAME} ${ARG_HDRS} ${ARG_SRCS} ${ARG_RSRCS}) # add executable with given name, headers, sources and resources
   target_link_libraries(${ARG_NAME} PRIVATE ${ARG_DEPS}) # link library with given dependencies
@@ -310,6 +362,14 @@ function(robocin_cpp_executable)
   target_compile_definitions(${ARG_NAME} PRIVATE ROBOCIN_PROJECT_NAME="${ROBOCIN_PROJECT_NAME}")
   target_compile_definitions(${ARG_NAME} PRIVATE ROBOCIN_PROJECT_PATH="${ROBOCIN_PROJECT_PATH}")
   target_compile_definitions(${ARG_NAME} PRIVATE ROBOCIN_CONFIG_PATH="${ROBOCIN_CONFIG_PATH}")
+
+  if (ARG_MACROS)
+    target_compile_definitions(${ARG_NAME} ${ARG_MACROS})
+  endif ()
+
+  if (ARG_COMPILE_OPTIONS)
+    target_compile_options(${ARG_NAME} ${ARG_COMPILE_OPTIONS})
+  endif ()
 
 endfunction(robocin_cpp_executable)
 
