757ace7c949014d4853a3948925c3bac6d05587c
Matheus Teotonio <matheusvncs160@gmail.com>
2023-06-18 21:01:15 -0300

    wip

--- ines/services/vision_main.cpp
+++ ines/services/vision_main.cpp
@@ -2,11 +2,17 @@
 #include <numbers>
 #include <ranges>
 
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+
 #include <absl/random/distributions.h>
 #include <absl/random/random.h>
 #include <absl/time/clock.h>
 #include <absl/time/time.h>
 
+#include <google/protobuf/io/coded_stream.h>
+#include <google/protobuf/io/zero_copy_stream_impl.h>
 #include <google/protobuf/timestamp.pb.h>
 
 #include "ines/common/publish_subscribe.h"
@@ -14,6 +20,7 @@
 #include "ines/common/zmq_publish_subscribe.h"
 #include "ines/services/common.pb.h"
 #include "ines/services/vision.pb.h"
+#include "ines/third_part/ssl_vision_wrapper.pb.h"
 
 #include "google/protobuf/util/time_util.h"
 
@@ -36,106 +43,56 @@ static constexpr float kGoalCenterToPenaltyMark = 6000.0F;
 
 static constexpr double k240HzInMs = 1000.0 / 240.0;
 
-absl::BitGen bitgen;
-
-float getRandomX() {
-  static constexpr float kHalfFieldLength = kFieldLength / 2.0F;
-  return absl::Uniform(bitgen, -kHalfFieldLength, kHalfFieldLength);
-}
-
-float getRandomY() {
-  static constexpr float kHalfFieldWidth = kFieldWidth / 2.0F;
-  return absl::Uniform(bitgen, -kHalfFieldWidth, kHalfFieldWidth);
-}
-
-float getRandomAngle() {
-  static constexpr float kPi = std::numbers::pi_v<float>;
-
-  return absl::Uniform(bitgen, -kPi, kPi);
-}
-
-Field getDivBField(uint64_t id) {
-  Field result;
-  result.set_id(id);
-  *result.mutable_timestamp() = timestampFromNanos(absl::GetCurrentTimeNanos());
-
-  result.set_length(kFieldLength);
-  result.set_width(kFieldWidth);
-  result.set_goal_depth(kGoalDepth);
-  result.set_goal_width(kGoalWidth);
-  result.set_penalty_area_depth(kPenaltyAreaDepth);
-  result.set_penalty_area_width(kPenaltyAreaWidth);
-  result.set_goal_center_to_penalty_mark(kGoalCenterToPenaltyMark);
-
-  return result;
-}
-
-Ball getMockedBall() {
-  Ball result;
-
-  result.mutable_position()->set_x(getRandomX());
-  result.mutable_position()->set_y(getRandomY());
-
-  result.mutable_velocity()->set_x(0.0F);
-  result.mutable_velocity()->set_y(0.0F);
-
-  return result;
-}
-
-Robot getMockedRobot(int id) {
-  Robot result;
-  result.set_id(id);
-
-  result.mutable_position()->set_x(getRandomX());
-  result.mutable_position()->set_y(getRandomY());
-
-  result.set_angle(getRandomAngle());
-
-  result.mutable_velocity()->set_x(0.0F);
-  result.mutable_velocity()->set_y(0.0F);
-
-  result.set_angular_velocity(0.0F);
-
-  return result;
-}
-
-Frame getMockedFrame(uint64_t id) {
-  Frame result;
-  result.set_id(id);
-  *result.mutable_timestamp() = timestampFromNanos(absl::GetCurrentTimeNanos());
-
-  // result.add_source_ids("robot_0_ir");
-  // result.add_source_ids("camera_1");
-  // result.add_source_ids("camera_2");
-  // result.add_source_ids("camera_3");
-  // result.add_source_ids("camera_4");
-
-  *result.mutable_ball() = getMockedBall();
-  for (int i : std::ranges::iota_view(0, 6)) {
-    *result.add_teammates() = getMockedRobot(i);
-    *result.add_opponents() = getMockedRobot(i);
-  }
-
-  return result;
-}
+constexpr int kBufferSize = 1024;
+constexpr int kPort = 10006;
 
 int main() {
   std::unique_ptr<ITopicPublisher> publisher = std::make_unique<ZmqPublisher>();
   publisher->bind("ipc:///tmp/vision.ipc");
 
-  for (uint64_t frame_id : std::ranges::iota_view<uint64_t>(0)) {
-    Frame frame = getMockedFrame(frame_id);
-    // *frame.mutable_timestamp() = getNow();
-    publisher->send("vision", PubSubMode::DontWait, frame);
-
-    if (frame_id % 120 == 0) { // Send field every 120 frames
-      Field field = getDivBField(frame_id / 120);
-      //*field.mutable_timestamp() = getNow();
-      publisher->send("field", PubSubMode::DontWait, field);
-    }
-
-    absl::SleepFor(absl::Milliseconds(k240HzInMs));
+  int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+  if (sockfd < 0) {
+    std::cerr << "Failed to create socket." << std::endl;
+    return 1;
   }
 
+  sockaddr_in server_address{};
+  server_address.sin_family = AF_INET;
+  server_address.sin_addr.s_addr = inet_addr("192.168.1.18");
+  server_address.sin_port = htons(kPort);
+
+  if (bind(sockfd,
+           static_cast<sockaddr*>(static_cast<void*>(&server_address)),
+           sizeof(server_address)) < 0) {
+    std::cerr << "Failed to bind socket." << std::endl;
+    return 1;
+  }
+
+  std::array<char, kBufferSize> buffer{};
+  sockaddr_in client_address{};
+  socklen_t client_len = sizeof(client_address);
+
+  while (true) {
+    ssize_t received = recvfrom(sockfd,
+                                &buffer,
+                                kBufferSize,
+                                0,
+                                static_cast<sockaddr*>(static_cast<void*>(&client_address)),
+                                &client_len);
+    if (received < 0) {
+      std::cerr << "Error in receiving data." << std::endl;
+      continue;
+    }
+
+    SSL_WrapperPacket packet;
+    if (!packet.ParseFromArray(buffer, received)) {
+      std::cerr << "Failed to parse SSL_WrapperPacket." << std::endl;
+      continue;
+    }
+
+    std::cout << "Number of detected robots: " << packet.detection().robots().size() << std::endl;
+  }
+
+  close(sockfd);
   return 0;
 }
